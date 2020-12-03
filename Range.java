667fdd4429eb642bf07e93bf6c115aa93317bf7d
Harry Pan <hpan10@stuy.edu>
2020-12-03 17:26:47 -0500

    I've completed all required tasks for assignment 24

---
+++ Range.java
@@ -0,0 +1,37 @@
+import java.util.NoSuchElementException;
+
+public class Range implements IntegerSequence{
+
+  public int start, end, current;
+
+  public Range(int start, int end){
+    if (start > end){
+      throw new IllegalArgumentException(start + " cannot be larger than " + end + "!");
+    }
+    this.start = start;
+    this.end = end;
+    current = start;
+  }
+
+  public void reset(){
+    current = start;
+  }
+
+  public int length(){
+    return end - start + 1;
+  }
+
+  public boolean hasNext(){
+    return current <= end;
+  }
+
+  public int next(){
+    if (current > end){
+      throw new NoSuchElementException("No more elements in Range(" + start + ", " + end + ")");
+    }
+    int temp = current;
+    current++;
+    return temp;
+  }
+
+}
