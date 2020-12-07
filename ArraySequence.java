227336f1ebdd3bb4e6317731893902e25970702f
Harry Pan <hpan10@stuy.edu>
2020-12-07 12:53:48 -0500

    I've implemented all appropriate methods and constructors, but there are a few things that need work

---
+++ ArraySequence.java
@@ -0,0 +1,41 @@
+import java.util.NoSuchElementException;
+
+public class ArraySequence implements IntegerSequence{
+
+  int currentIndex;
+  int[] data;
+
+  public ArraySequence(int[] other){
+    data = other;
+    currentIndex = 0;
+  }
+
+  public ArraySequence(IntegerSequence otherseq){
+    data = new int[otherseq.length()];
+    for (int i = 0; otherseq.hasNext(); i++){
+      data[i] = otherseq.next();
+    }
+    otherseq.reset();
+  }
+
+  public boolean hasNext(){
+    return currentIndex < data.length;
+  }
+
+  public int next(){
+    if (!(hasNext())){
+      throw new NoSuchElementException("No more elements");
+    }
+    int temp = data[currentIndex];
+    currentIndex++;
+    return temp;
+  }
+  public int length(){
+    return data.length;
+  }
+
+  public void reset(){
+    currentIndex = 0;
+  }
+
+}
